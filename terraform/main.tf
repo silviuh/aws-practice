4f08c0d404c63678b3d83278f6de7a8a055c10b7
silviuh <silviuhartan10@gmail.com>
2024-08-16 10:43:52 +0300

    Terraform provisioning added

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,28 +15,49 @@ resource "aws_s3_bucket_public_access_block" "lambda_bucket_access" {
   restrict_public_buckets = true
 }
 
-data "archive_file" "lambda_package" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src"
-  output_path = "${path.module}/../tmp/lambda_package.zip"
-  excludes    = ["__pycache__", "*.pyc"]
+resource "null_resource" "create_lambda_package" {
+  triggers = {
+    src_hash     = sha256(join("", [for f in fileset("${path.module}/../src", "**"): filesha256("${path.module}/../src/${f}")]))
+    requirements = filemd5("${path.module}/../requirements.txt")
+    always_run   = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p ${path.module}/package
+      cp -R ${path.module}/../src/* ${path.module}/package/
+      pip install -r ${path.module}/../requirements.txt -t ${path.module}/package/
+      cd ${path.module}/package
+      echo "Package contents:"
+      ls -R
+      zip -r ${path.module}/lambda_package.zip .
+      aws s3 cp ${path.module}/lambda_package.zip s3://${aws_s3_bucket.lambda_bucket.id}/lambda_package_$(date +%s).zip
+    EOT
+  }
 }
 
-resource "aws_s3_object" "lambda_package" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "lambda_package_${timestamp()}.zip"
-  source = data.archive_file.lambda_package.output_path
-  etag   = filemd5(data.archive_file.lambda_package.output_path)
+data "aws_s3_bucket_objects" "lambda_packages" {
+  bucket     = aws_s3_bucket.lambda_bucket.id
+  prefix     = "lambda_package_"
+  depends_on = [null_resource.create_lambda_package]
+}
+
+data "aws_s3_object" "lambda_package" {
+  bucket     = aws_s3_bucket.lambda_bucket.id
+  key        = element(sort(data.aws_s3_bucket_objects.lambda_packages.keys), length(data.aws_s3_bucket_objects.lambda_packages.keys) - 1)
+  depends_on = [null_resource.create_lambda_package]
 }
 
 resource "aws_lambda_function" "slack_command" {
-  function_name = "slack-command-lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
+  function_name    = "slack-command-lambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  s3_bucket        = data.aws_s3_object.lambda_package.bucket
+  s3_key           = data.aws_s3_object.lambda_package.key
+  source_code_hash = data.aws_s3_object.lambda_package.body
 
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_package.key
+  depends_on = [null_resource.create_lambda_package]
 
   environment {
     variables = {
@@ -47,19 +68,37 @@ resource "aws_lambda_function" "slack_command" {
 }
 
 resource "aws_lambda_function" "initialize_environments" {
-  function_name = "initialize-environments-lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "initialize_environments_lambda.lambda_handler"
-  runtime       = "python3.8"
+  function_name    = "initialize-environments-lambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "initialize_environments_lambda.lambda_handler"
+  runtime          = "python3.8"
+  s3_bucket        = data.aws_s3_object.lambda_package.bucket
+  s3_key           = data.aws_s3_object.lambda_package.key
+  source_code_hash = data.aws_s3_object.lambda_package.body
 
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_package.key
+  depends_on = [null_resource.create_lambda_package]
 
   environment {
     variables = {
       DYNAMODB_TABLE = aws_dynamodb_table.environments_table.name
     }
   }
+}
+
+resource "aws_lambda_permission" "slack_command_permission" {
+  statement_id  = "AllowAPIGatewayInvokeSlackCommand"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.slack_command.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.slack_api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "initialize_permission" {
+  statement_id  = "AllowAPIGatewayInvokeInitialize"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.initialize_environments.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.slack_api.execution_arn}/*/*"
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -110,7 +149,7 @@ resource "aws_iam_role_policy" "dynamodb_policy" {
 }
 
 resource "aws_api_gateway_rest_api" "slack_api" {
-  name = "slack-command-api"
+  name = "slack-command-api-v2"
 }
 
 resource "aws_api_gateway_resource" "slack_resource" {
@@ -119,9 +158,22 @@ resource "aws_api_gateway_resource" "slack_resource" {
   path_part   = "slack-command"
 }
 
+resource "aws_api_gateway_resource" "initialize_resource" {
+  rest_api_id = aws_api_gateway_rest_api.slack_api.id
+  parent_id   = aws_api_gateway_rest_api.slack_api.root_resource_id
+  path_part   = "initialize"
+}
+
 resource "aws_api_gateway_method" "slack_method" {
   rest_api_id   = aws_api_gateway_rest_api.slack_api.id
   resource_id   = aws_api_gateway_resource.slack_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "initialize_method" {
+  rest_api_id   = aws_api_gateway_rest_api.slack_api.id
+  resource_id   = aws_api_gateway_resource.initialize_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
@@ -134,6 +186,47 @@ resource "aws_api_gateway_integration" "slack_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.slack_command.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "initialize_integration" {
+  rest_api_id = aws_api_gateway_rest_api.slack_api.id
+  resource_id = aws_api_gateway_resource.initialize_resource.id
+  http_method = aws_api_gateway_method.initialize_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.initialize_environments.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api" {
+  depends_on = [
+    aws_api_gateway_integration.slack_integration,
+    aws_api_gateway_integration.initialize_integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.slack_api.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.slack_resource.id,
+      aws_api_gateway_method.slack_method.id,
+      aws_api_gateway_integration.slack_integration.id,
+
+      aws_api_gateway_resource.initialize_resource.id,
+      aws_api_gateway_method.initialize_method.id,
+      aws_api_gateway_integration.initialize_integration.id,
+    ]))
+  }
+}
+
+resource "aws_api_gateway_stage" "api" {
+  deployment_id = aws_api_gateway_deployment.api.id
+  rest_api_id   = aws_api_gateway_rest_api.slack_api.id
+  stage_name    = "prod"
 }
 
 resource "aws_dynamodb_table" "environments_table" {
---
+++ terraform/package/initialize_environments_lambda.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+# Debugging information
+print("Current working directory:", os.getcwd())
+print("Contents of current directory:", os.listdir())
+print("Python path:", sys.path)
+print("Environment variables:", dict(os.environ))
+
+try:
+    from dynamo_operations import initialize_environments
+    print("Successfully imported initialize_environments from dynamo_operations")
+except ImportError as e:
+    print(f"Error importing initialize_environments: {e}")
+
+def lambda_handler(event, context):
+    print(event)
+    print(event['body'])
+
+    try:
+        initialize_environments(env_count=5)
+        return {
+            'statusCode': 200,
+            'body': 'Environment initialization complete.'
+        }
+    except Exception as e:
+        print(f"Error during initialization: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': f'Error during initialization: {str(e)}'
+        }
